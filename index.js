81ae23ca24e82f940f676278c7d5e7f5c8e9c791
Martin Macharia <nate@Martins-MacBook-Pro.local>
2016-08-12 16:02:05 +0300

    basic express server

---
+++ index.js
@@ -0,0 +1,32 @@
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  require('dotenv').load();
+}
+// The main application script, ties everything together.
+var bodyParser = require('body-parser');
+var config = require('./config.js')[env];
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/app'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {
+    root: './'
+  });
+});
+
+var server = app.listen(config.port, function() {
+  console.log('Express server listening on %d, in %s' +
+    ' mode', server.address().port, app.get('env'));
+});
+
+module.exports = app;
