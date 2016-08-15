dfa666da6ca5f9b8cb3c00f795ed30b4ef8da2d1
Martin Macharia <nate@Martins-MacBook-Pro.local>
2016-08-15 16:22:43 +0300

    adds the x limits

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,11 @@ var ctx;
 var dx = 5;
 var dy = 5;
 var x = 0;
-var y = Math.random() * 150;
+var xpoint = 0;
+var xpos = 0;
+var starty = Math.random() * 150;
+var endy = Math.random() * 150;
+var diff = (endy - starty)/60;
 var righty = 65
 var lefty = 65;
 var WIDTH = 300;
@@ -19,13 +23,13 @@ function divider() {
 
 // rendering the left bat
 function leftBat(y) {
-  ctx.fillStyle = '#F30';
+  ctx.fillStyle = '#000';
   ctx.fillRect(0, y, 4, 20);
 }
 
 // rendering the right bat
 function rightBat(y) {
-  ctx.fillStyle = '#F30';
+  ctx.fillStyle = '#000';
   ctx.fillRect(296, y, 4, 20);
 }
 
@@ -33,37 +37,47 @@ function rightBat(y) {
 function ball(x, y, r) {
   ctx.beginPath();
   ctx.arc(x, y, r, 0, Math.PI * 2, true);
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = '#f00';
   ctx.fill();
   ctx.lineWidth = 1;
   ctx.strokeStyle = 'black';
   ctx.stroke();
 }
 
+var possiblePoints = [];
+for (var i = 0; i <= 300/dy; i++) {
+  possiblePoints.push(i * dy);
+}
+
+/* The ball needs to move from any random point on
+the left side of the table in any random direction.
+If it does not intercept any of the bats then it should
+exit the round. */
+
 // to handle the key press events for the bats
 function doKeyDown(evt) {
   switch (evt.keyCode) {
     case 38:
       /* Up arrow was pressed */
-      if (righty - dy > 0) {
+      if (righty - dy > -5) {
         righty -= dy;
       }
       break;
     case 40:
       /* Down arrow was pressed */
-      if (righty + dy < HEIGHT) {
+      if (righty + dy < HEIGHT-15) {
         righty += dy;
       }
       break;
     case 87:
       /* W key was pressed */
-      if (lefty - dx > 0) {
+      if (lefty - dx > -5) {
         lefty -= dx;
       }
       break;
     case 83:
       /* S key was pressed */
-      if (lefty + dx < WIDTH) {
+      if (lefty + dx < HEIGHT-15) {
         lefty += dx;
       }
       break;
@@ -74,7 +88,7 @@ function doKeyDown(evt) {
 function init() {
   canvas = document.getElementById('play-field');
   ctx = canvas.getContext('2d');
-  return setInterval(draw, 10);
+  return setInterval(draw, 20);
 }
 
 // rendering all the components
@@ -86,7 +100,19 @@ function draw() {
   leftBat(lefty);
   rightBat(righty);
   ctx.fillStyle = 'black';
-  ball(x, y, 5);
+  // move the ball
+  console.log(diff);
+  xpos = x % 120;
+  if (xpos <= 60) {
+    xpoint = x % 60;
+    starty = starty + diff;
+    ball(possiblePoints[xpoint], starty + diff, 5);
+  } else {
+    xpoint = (60 - x % 60);
+    starty = starty + diff;
+    ball(possiblePoints[xpoint], starty + diff, 5);
+  }
+  x = x + 1;
 }
 
 // clear the canvas
